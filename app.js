fa99c88bbf46f4c98956ed88daddddc29ece1744
PGK16POWERS <136324051+PGK16POWERS@users.noreply.github.com>
2025-02-24 19:53:14 +0200

    js

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 const express = require("express");
 const app = express();
+const path = require("path")
 require("dotenv").config()
 
-app.use(express.urlencoded({extended: true}))
-app.use(express.json())
+app.use(express.urlencoded({extended : true}));
+app.use(express.json());
+app.use(express.static("heavy-weight/browser"));
 
-app.get("/", (req, res) => {
-    res.status(200).json(message = "Heavy Weight");
+app.get("*", (req, res) => {
+    res.status(200).sendFile(path.join(__dirname,"/heavy-weight/browser/index.html"));
 })
 
 app.listen(process.env.PORT, () => {
